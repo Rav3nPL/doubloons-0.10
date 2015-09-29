3dda68f2a7c8eb682383312362b5c719e003bc1c
Rav3n_pl <rav3n.pl@gmail.com>
2015-09-29 13:18:33 +0200

    Fix some RPC calls LTC->DBL

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -309,11 +309,11 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-            "dumpprivkey \"litecoinaddress\"\n"
-            "\nReveals the private key corresponding to 'litecoinaddress'.\n"
+            "dumpprivkey \"doubloonsaddress\"\n"
+            "\nReveals the private key corresponding to 'doubloonsaddress'.\n"
             "Then the importprivkey can be used with this output\n"
             "\nArguments:\n"
-            "1. \"litecoinaddress\"   (string, required) The litecoin address for the private key\n"
+            "1. \"doubloonsaddress\"   (string, required) The litecoin address for the private key\n"
             "\nResult:\n"
             "\"key\"                (string) The private key\n"
             "\nExamples:\n"
